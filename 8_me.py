30cb15b612a9b93a954597110dde2593206690ed
Keunbae Park <pkboom@users.noreply.github.com>
2023-01-20 22:43:02 -0500

    wip

--- 8_me.py
+++ 8_me.py
@@ -1,99 +1,55 @@
 with open("8.in") as f:
     lines = [l.strip() for l in f]
 
-visible = []
-scenic = []
+R = len(lines)
+C = len(lines[0])
 
 
-def in_forest(lines, xp, yp, direction, count):
-    if direction == "left":
-        position = yp - 1 * count
-        return position, position >= 0
-    elif direction == "right":
-        position = yp + 1 * count
-        return (
-            position,
-            position < len(lines[0]),
-        )
-    elif direction == "up":
-        position = xp - 1 * count
-        return position, position >= 0
-    else:
-        position = xp + 1 * count
-        return (
-            position,
-            position < len(lines),
-        )
-
-
-def get_value(lines, xp, yp, direction, position):
-    if direction == "left":
-        return lines[xp][position]
-    elif direction == "right":
-        return lines[xp][position]
-    elif direction == "up":
-        return lines[position][yp]
-    else:
-        return lines[position][yp]
-
-
-def is_blocking(lines, yv, yp, xv, xp):
-    for direction in ["left", "right", "up", "down"]:
-        c = 1
-
-        while True:
-            position, inside_forest = in_forest(lines, xp, yp, direction, c)
-
-            if not inside_forest:
-                return False
-
-            value = get_value(lines, xp, yp, direction, position)
-
-            if value >= yv:
+p1 = 0
+for r in range(R):
+    for c in range(C):
+        t = lines[r][c]
+        visible = False
+        for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            rr = r
+            cc = c
+            while True:
+                rr = rr + dr
+                cc = cc + dc
+                if 0 <= rr < R and 0 <= cc < C:
+                    if t <= lines[rr][cc]:
+                        break
+                else:
+                    visible = True
+                    p1 += 1
+                    break
+            if visible:
                 break
 
-            c += 1
 
-    return True
+print(p1)
 
-
-def get_scenic_score(lines, yv, yp, xv, xp):
-    score = 1
-
-    for direction in ["left", "right", "up", "down"]:
-        c = 1
-
-        while True:
-            position, inside_forest = in_forest(lines, xp, yp, direction, c)
-
-            if not inside_forest:
-                print("inside_forest c", c)
-                score *= c - 1
-                break
-
-            value = get_value(lines, xp, yp, direction, position)
-
-            if value >= yv:
-                print("value >= yv c", c)
-                score *= c
-                break
-
-            c += 1
-
-    return score
+p2 = 0
+for r in range(R):
+    for c in range(C):
+        score = 1
+        t = lines[r][c]
+        for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            rr = r
+            cc = c
+            v = 1
+            while True:
+                rr = rr + dr
+                cc = cc + dc
+                if 0 <= rr < R and 0 <= cc < C:
+                    if t <= lines[rr][cc]:
+                        score *= v
+                        break
+                else:
+                    score *= v - 1
+                    break
+                v += 1
+        p2 = max(p2, score)
 
 
-for xp in range(len(lines)):
-    xv = lines[xp]
-
-    for yp in range(len(xv)):
-        yv = xv[yp]
-
-        if not is_blocking(lines, yv, yp, xv, xp):
-            visible.append((xp, yp))
-
-        scenic.append(get_scenic_score(lines, yv, yp, xv, xp))
-
-print(len(set(visible)))
-print(scenic)
-print(max(scenic))
+print(p2)
