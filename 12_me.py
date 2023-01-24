e3eedaf57f6733e102d53c55e07d4667ff500e8d
Keunbae Park <pkboom@users.noreply.github.com>
2023-01-24 14:54:53 -0500

    wip

--- 12_me.py
+++ 12_me.py
@@ -3,40 +3,72 @@ from copy import deepcopy
 with open("12.in") as f:
     lines = [l.strip() for l in f]
 
-for l in lines:
-    print(l)
-
+H = []
+P = []
+x = 0
+y = 0
 r_len = len(lines)
 c_len = len(lines[0])
+h = [(x, y)]
+hp = []
+prev = []
+d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+p = ["V", ">", "^", "<"]
+found_e = False
+
+while True:
+    next = ()
+
+    for i, dd in enumerate(d):
+        r, c = dd
+        rr, cc = x + r, y + c
+
+        if rr < 0 or cc < 0 or rr >= r_len or cc >= c_len:
+            continue
+
+        if (rr, cc) in h:
+            continue
+
+        h_copied = deepcopy(h)
+        h_copied.append((rr, cc))
+        if h_copied in prev:
+            continue
+
+        if (ord(lines[rr][cc]) - ord(lines[x][y])) == 1:
+            next = (rr, cc)
+            break
+
+        if lines[rr][cc] == "E":
+            break
+
+        if not next and (ord(lines[rr][cc]) - ord(lines[x][y])) <= 0:
+            next = (rr, cc)
+            continue
+
+    print("next", next)
+    print("prev", prev)
+    if found_e and lines[rr][cc] == "E":
+        steps = deepcopy(h)
+        H.append(steps)
+        print("steps", steps)
+        print("-------------------------------")
+
+    if next:
+        x, y = next
+        if lines[x][y] == "e":
+            found_e = True
+        h.append((x, y))
+        print("history", h)
+        print("x, y", x, y)
+        print("character", lines[x][y])
+    else:
+        prev.append(deepcopy(h))
+        h.pop()
+        if len(h) == 0:
+            break
+        x, y = h[-1]
+        print("backward x, y", x, y)
+        print("backward history", h)
 
 
-def move(r, c, r_len, c_len, history):
-    print("history", history)
-    rr = r + 1
-    cc = c + 1
-
-    if rr >= r_len or cc >= c_len:
-        history.append((r, c, "X"))
-        return history
-
-    print(lines[r][c])
-    print(lines[rr][c])
-    print(lines[r][cc])
-
-    if lines[r][c] == "c":
-        history.append((r, c, "V"))
-        return history
-
-    if 0 <= (ord(lines[rr][c]) - ord(lines[r][c])) <= 1:
-        history.append((r, c, "V"))
-        return move(rr, c, r_len, c_len, history)
-    if 0 <= (ord(lines[r][cc]) - ord(lines[r][c])) <= 1:
-        history.append((r, c, ">"))
-        return move(r, cc, r_len, c_len, history)
-
-
-r = 0
-c = 0
-
-h = move(r, c, r_len, c_len, [])
-print(h)
+print("H", H)
