43a673475d5bc963b33948ca6f6017a0fe11e70b
Keunbae Park <pkboom@users.noreply.github.com>
2023-01-24 22:33:16 -0500

    wip

--- 12_me.py
+++ 12_me.py
@@ -9,15 +9,15 @@ x = 0
 y = 0
 r_len = len(lines)
 c_len = len(lines[0])
-h = [(x, y)]
 hp = []
 prev = []
 d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 p = ["V", ">", "^", "<"]
 found_e = False
 
-while True:
-    next = ()
+
+def move(x, y, h=[]):
+    next = [[], [], [], []]
 
     for i, dd in enumerate(d):
         r, c = dd
@@ -29,46 +29,26 @@ while True:
         if (rr, cc) in h:
             continue
 
-        h_copied = deepcopy(h)
-        h_copied.append((rr, cc))
-        if h_copied in prev:
-            continue
-
         if (ord(lines[rr][cc]) - ord(lines[x][y])) == 1:
-            next = (rr, cc)
-            break
+            next[i] = move(rr, cc, h + [(rr, cc)])
+        elif lines[rr][cc] == "E":
+            return h + [(rr, cc)]
+        elif (ord(lines[rr][cc]) - ord(lines[x][y])) <= 0:
+            next[i] = move(rr, cc, h + [(rr, cc)])
 
-        if lines[rr][cc] == "E":
-            break
+    next = [n for n in next if n]
+    if not next:
+        return []
+    l = 99999
+    key = 0
+    for i, n in enumerate(next):
+        if len(n) < l:
+            l = len(n)
+            key = i
 
-        if not next and (ord(lines[rr][cc]) - ord(lines[x][y])) <= 0:
-            next = (rr, cc)
-            continue
-
-    print("next", next)
-    print("prev", prev)
-    if found_e and lines[rr][cc] == "E":
-        steps = deepcopy(h)
-        H.append(steps)
-        print("steps", steps)
-        print("-------------------------------")
-
-    if next:
-        x, y = next
-        if lines[x][y] == "e":
-            found_e = True
-        h.append((x, y))
-        print("history", h)
-        print("x, y", x, y)
-        print("character", lines[x][y])
-    else:
-        prev.append(deepcopy(h))
-        h.pop()
-        if len(h) == 0:
-            break
-        x, y = h[-1]
-        print("backward x, y", x, y)
-        print("backward history", h)
+    return next[key]
 
 
-print("H", H)
+path = move(x, y, [(x, y)])
+
+print("path", path)
