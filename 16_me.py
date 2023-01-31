2768576924bce77003299ec59d5320ca1e433515
Keunbae Park <pkboom@users.noreply.github.com>
2023-01-31 16:12:22 -0500

    wip

--- 16_me.py
+++ 16_me.py
@@ -9,26 +9,35 @@ for l in lines:
     scan = l.split()
     v, r, t = scan[1], int(scan[4][5:-1]), [_.strip(",") for _ in scan[9:]]
     S[v] = [r, t]  # rate, tunnel
+    print(v, S[v])
 
-Q = [[0, "AA", 0, False]]
+Q = [[0, "AA", 0, [], []]]
 M = 0
 q = []
 
 while len(Q) > 0:
-    c, v, p, all_open, *open = Q.pop()
-    if c >= 30 or all_open:
+    c, v, p, open, path = Q.pop()
+    if c >= 30:
         M = max(M, p)
         print(len(Q), M)
         continue
-    if v not in open:
-        open = open + [v]
-        if S[v][0] != 0:
-            c += 1
-            p += S[v][0] * (30 - c)
-        if len(open) == len(S):
-            all_open = True
 
+    stuck = True
     for t in S[v][1]:
-        Q.append([c + 1, t, p, all_open, *open])
+        if (v, t) not in path:
+            Q.append([c + 1, t, p, open + [v], path + [(v, t)]])
+            if v not in open and S[v][0] != 0:
+                Q.append(
+                    [
+                        c + 2,
+                        t,
+                        p + S[v][0] * (30 - (c + 1)),
+                        open + [v],
+                        path + [(v, t)],
+                    ]
+                )
+            stuck = False
+    if stuck:
+        M = max(M, p)
 
 print(M)
