10fc88d75cfbe6c90026691a7e47dc3b47c9b177
Keunbae Park <pkboom@users.noreply.github.com>
2023-02-01 14:19:51 -0500

    wip

--- 16_me.py
+++ 16_me.py
@@ -1,43 +1,51 @@
-from collections import deque
 from copy import deepcopy
 
 with open("16.in") as f:
     lines = [l.strip() for l in f]
 
 S = {}
+b = 1
+all_open = 0
 for l in lines:
     scan = l.split()
     v, r, t = scan[1], int(scan[4][5:-1]), [_.strip(",") for _ in scan[9:]]
-    S[v] = [r, t]  # rate, tunnel
+    S[v] = [r, t, b]  # rate, tunnel
     print(v, S[v])
+    all_open |= b
+    b <<= 1
 
-Q = [[0, "AA", 0, [], []]]
+Q = [
+    [0, "AA", 0, 0, "AA", ""]
+]  # count, current value, pressure, open, path, duplicate path
 M = 0
-q = []
 
 while len(Q) > 0:
-    c, v, p, open, path = Q.pop()
+    c, v, p, open, path, duplicate = Q.pop()
+    if (open & all_open) == all_open:
+        M = max(M, p)
+        continue
     if c >= 30:
         M = max(M, p)
-        print(len(Q), M)
         continue
 
-    stuck = True
     for t in S[v][1]:
-        if (v, t) not in path:
-            Q.append([c + 1, t, p, open + [v], path + [(v, t)]])
-            if v not in open and S[v][0] != 0:
-                Q.append(
-                    [
-                        c + 2,
-                        t,
-                        p + S[v][0] * (30 - (c + 1)),
-                        open + [v],
-                        path + [(v, t)],
-                    ]
-                )
-            stuck = False
-    if stuck:
-        M = max(M, p)
+        if path + t == duplicate:
+            M = max(M, p)
+            continue
+        if t in path:
+            duplicate = path
+            path = ""
+        Q.append([c + 1, t, p, open | S[v][2], path + t, duplicate])
+        if (S[v][2] & open) == 0 and S[v][0] != 0:
+            Q.append(
+                [
+                    c + 2,
+                    t,
+                    p + S[v][0] * (30 - (c + 1)),
+                    open | S[v][2],
+                    path + t,
+                    duplicate,
+                ]
+            )
 
 print(M)
