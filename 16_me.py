4f9f844390a3b8567517172bcdef1f3c644f75c6
Keunbae Park <pkboom@users.noreply.github.com>
2023-02-02 09:13:21 -0500

    wip

--- 16_me.py
+++ 16_me.py
@@ -2,68 +2,52 @@ from collections import deque
 
 rates = {}
 tunnels = {}
-b = 1
-for line in open("16.in"):
+bitmask = {}
+for b, line in enumerate(open("16.in")):
     scan = line.split()
     v, r, t = scan[1], int(scan[4][5:-1]), [_.strip(",") for _ in scan[9:]]
-    rates[v] = r
+    rates[v] = int(r)
     tunnels[v] = t
-    # all_open |= b
-    # b <<= 1
+    bitmask[v] = 1 << b
 
-# caculate the distance from values to every other value with pressure
 dist = {}
-for valve, rate in rates.items():
+for valve, rate in [r for r in rates.items() if r[1] != 0 or r[0] == "AA"]:
     dist[valve] = {}
-    print(valve, rate)
-    queue = deque([(valve, valve, 0)])
+    queue = deque([(valve, valve, bitmask[valve], 0)])
     while queue:
-        origin, current, distance = queue.popleft()
+        origin, current, b, distance = queue.popleft()
         for t in tunnels[current]:
-            if t == "AA" or t in dist[origin]:
+            if b & bitmask[t] != 0:
                 continue
-            # if rates[t] == 0:
-            #     continue
-            dist[origin][t] = distance + 1
-            queue.append((origin, t, distance + 1))
+            if t in dist[origin]:
+                continue
+            if rates[t] != 0:
+                dist[origin][t] = distance + 1
+            queue.append((origin, t, b | bitmask[t], distance + 1))
 
-# print(dist)
-# b = 1
-# all_open = 0
-# for l in lines:
+all_open = 1
+for valve in dist["AA"]:
+    all_open = all_open | bitmask[valve]
+print(all_open)
+for valve, distance in dist.items():
+    print(valve, distance)
+time = 30
+pressures = []
+for valve, distance in dist["AA"].items():
+    queue = [
+        (time - distance, valve, bitmask[valve] | bitmask["AA"], 0)
+    ]  # time, valve, bit, pressure
+    while queue:
+        time, current, b, pressure = queue.pop()
+        if time <= 0 or b & all_open == all_open:
+            pressures.append(pressure)
+            print(current, time, pressure)
+            continue
+        p = (time - 1) * rates[current]
+        for t in dist[current].keys():
+            if b & bitmask[t] == 0:
+                queue.append(
+                    (time - 1 - dist[current][t], t, b | bitmask[t], p + pressure)
+                )
 
-# Q = [
-#     [0, "AA", 0, 0, "AA", ""]
-# ]  # count, current value, pressure, open, path, duplicate path
-# M = 0
-
-# while len(Q) > 0:
-#     c, v, p, open, path, duplicate = Q.pop()
-#     if (open & all_open) == all_open:
-#         M = max(M, p)
-#         continue
-#     if c >= 30:
-#         M = max(M, p)
-#         continue
-
-#     for t in S[v][1]:
-#         if path + t == duplicate:
-#             M = max(M, p)
-#             continue
-#         if t in path:
-#             duplicate = path
-#             path = ""
-#         Q.append([c + 1, t, p, open | S[v][2], path + t, duplicate])
-#         if (S[v][2] & open) == 0 and S[v][0] != 0:
-#             Q.append(
-#                 [
-#                     c + 2,
-#                     t,
-#                     p + S[v][0] * (30 - (c + 1)),
-#                     open | S[v][2],
-#                     path + t,
-#                     duplicate,
-#                 ]
-#             )
-
-# print(M)
+print(max(pressures))
