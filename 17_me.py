223839dd496363cb44a7d80e56858ec15f1d36f1
Keunbae Park <pkboom@users.noreply.github.com>
2023-02-09 13:16:54 -0500

    wip

--- 17_me.py
+++ 17_me.py
@@ -5,9 +5,9 @@ LD = len(D)
 R = [
     [0b0011110],
     [0b0001000, 0b0011100, 0b0001000],
-    [0b0000100, 0b0000100, 0b0011100],
-    [0b0010000, 0b0010000, 0b0010000, 0b0010000],
-    [0b0011000, 0b0011000],
+    # [0b0000100, 0b0000100, 0b0011100],
+    # [0b0010000, 0b0010000, 0b0010000, 0b0010000],
+    # [0b0011000, 0b0011000],
 ]
 C1 = [0b1111111]
 C2 = []
@@ -31,15 +31,12 @@ def move(d):
         part_of_rock = C2[0][l]
         if d == "<":
             new_r = part_of_rock * 2
-            # left wall
             if new_r & 0b10000000:
                 return
         else:
             new_r = part_of_rock // 2
-            # right wall
             if part_of_rock & 1:
                 return
-        #  overlap with existing rock?
         if C1[C2[1] + l] & new_r:
             return
         R2.append(new_r)
@@ -50,13 +47,10 @@ def move(d):
 def down():
     for l in range(len(C2[0])):
         if C1[C2[1] + 1 + l] & C2[0][l]:
-            # hit bottom, you stop
-            # move rock from C2 to C1
             for l in range(len(C2[0])):
                 C1[C2[1] + l] |= C2[0][l]
             return True
     else:
-        # move down
         C2[1] += 1
         return False
 
@@ -65,30 +59,42 @@ def get_direction(dc):
     return D[dc % LD]
 
 
+p1 = 2022
+# p1 = 13
+p2 = 1e12
+# get cycle
+cycle = LD * len(R)
 rc = 0
 dc = 0
 while True:
-    # put rock in chamber2
-    C2 = [R[rc % 5].copy(), 0]
+    C2 = [R[rc % len(R)].copy(), 0]
     for i in range(len(C1)):
         if C1[i] != 0:
             break
-    if rc == 2022:
+    # if rc == cycle + 1:
+    #     rocks_height = len(C1) - i - 1
+    #     prev_rock_height = len(R[(rc - 1) % len(R)])
+    #     one_cycle_height = rocks_height - prev_rock_height
+    #     height = (p1 // cycle - 1) * one_cycle_height
+    #     p1 = p1 % cycle
+    #     rc = 1
+    if rc == p1:
+        # if rc == p2:
         print(len(C1) - i - 1)
+        # print(len(C1) - i)
+        # print("height", height)
         break
-    if i <= len(R[rc % 5]) + 2:
-        for _ in range(len(R[rc % 5]) + 3 - i):
+    if i <= len(R[rc % len(R)]) + 2:
+        for _ in range(len(R[rc % len(R)]) + 3 - i):
             C1.insert(0, 0b0000000)
     else:
-        for _ in range(i - (3 + len(R[rc % 5]))):
+        for _ in range(i - (3 + len(R[rc % len(R)]))):
             C1.remove(0)
-    while True:  # start moving, if hit bottom, stop
-        # draw_chamber()
+    while True:
         d = get_direction(dc)
         move(d)
         dc += 1
         # draw_chamber()
-        # down
         if down():
             break
         # draw_chamber()
