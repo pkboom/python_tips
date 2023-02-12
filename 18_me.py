40b469cfbe8f6c53298db00ac55a5d5a95d874e1
Keunbae Park <pkboom@users.noreply.github.com>
2023-02-12 15:48:58 -0500

    wip

--- 18_me.py
+++ 18_me.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 lines = open("18.in").read().strip()
 
 P = []
@@ -6,26 +8,66 @@ for line in lines.split("\n"):
     P.append((x, y, z))
 
 
-def reach_outside(x, y, z):
+OUT = set()
+IN = set()
+
+
+def reach_outside(x, y, z, part):
+    if (x, y, z) in OUT:
+        return True
+    if (x, y, z) in IN:
+        return False
     if (x, y, z) in P:
         return False
-    return True
+    if part == 1:
+        OUT.add((x, y, z))
+        return True
+    else:
+        Q = deque([(x, y, z)])
+        SEEN = set()
+        while Q:
+            x, y, z = Q.popleft()
+            if (x, y, z) in OUT:
+                continue
+            if (x, y, z) in P:
+                continue
+            SEEN.add((x, y, z))
+            if len(SEEN) > 5000:
+                # seen enough
+                #   legit
+                for p in SEEN:
+                    OUT.add(p)
+                return True
+
+            Q.append(x + 1, y, z)
+            Q.append(x - 1, y, z)
+            Q.append(x, y + 1, z)
+            Q.append(x, y - 1, z)
+            Q.append(x, y, z + 1)
+            Q.append(x, y, z - 1)
+        # inside
+        for p in SEEN:
+            IN.add(p)
+        return False
 
 
-ans = 0
-for x, y, z in P:
-    print(x, y, z)
-    if reach_outside(x + 1, y, z):
-        ans += 1
-    if reach_outside(x - 1, y, z):
-        ans += 1
-    if reach_outside(x, y + 1, z):
-        ans += 1
-    if reach_outside(x, y - 1, z):
-        ans += 1
-    if reach_outside(x, y, z + 1):
-        ans += 1
-    if reach_outside(x, y, z - 1):
-        ans += 1
+def solve(part):
+    ans = 0
+    for x, y, z in P:
+        if reach_outside(x + 1, y, z, part):
+            ans += 1
+        if reach_outside(x - 1, y, z, part):
+            ans += 1
+        if reach_outside(x, y + 1, z, part):
+            ans += 1
+        if reach_outside(x, y - 1, z, part):
+            ans += 1
+        if reach_outside(x, y, z + 1, part):
+            ans += 1
+        if reach_outside(x, y, z - 1, part):
+            ans += 1
+    return ans
 
-print(ans)
+
+# print(solve(1))
+print(solve(2))
