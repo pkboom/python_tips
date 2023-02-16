8999c29817fd833187097bec1c4106c01fc64f81
Keunbae Park <pkboom@users.noreply.github.com>
2023-02-16 11:41:44 -0500

    wip

--- 19_me.py
+++ 19_me.py
@@ -1,86 +1,42 @@
 from collections import deque
 
-lines = open("19.in").read().strip().split("\n")
 
-B = {}
-for line in lines:
-    b = [b.strip(":") for b in line.split(" ") if b.strip(":").isnumeric()]
-    # ore, clay, obsidian(ore), obsidian(clay), geode(ore), geode(obsidian)
-    B[b[0]] = (int(b[1]), int(b[2]), int(b[3]), int(b[4]), int(b[5]), int(b[6]))
-
-R = []
-total_geode = 0
-for k, v in B.items():
+def solve(Co, Cc, Co1, Co2, Cg1, Cg2, t):
     SEEN = set()
-    geo_max = 0
-    R.clear()
-    t = 23
-    Q = deque([(t, 1, 0, 0, 0, 1, 0, 0, 0)])
+    best = 0
+    Q = deque([(0, 0, 0, 0, 1, 0, 0, 0, t)])
     while Q:
-        t, m0, m1, m2, m3, r0, r1, r2, r3 = Q.popleft()
-        if (t, m0, m1, m2, m3, r0, r1, r2, r3) in SEEN:
+        state = Q.popleft()
+        o, c, ob, g, r0, r1, r2, r3, t = state
+        if t == 0:
+            best = max(best, g)
             continue
-        if t < 1:
-            geo_max = max(geo_max, m3)
+
+        if state in SEEN:
             continue
-        if m0 >= v[4] and m2 >= v[5]:
+        SEEN.add(state)
+
+        Q.append((t - 1, o + r0, c + r1, ob + r2, g + r3, r0, r1, r2, r3))
+        if o >= Co:
+            Q.append((t - 1, o - Co + r0, c + r1, ob + r2, g + r3, r0 + 1, r1, r2, r3))
+        if o >= Cc:
+            Q.append((t - 1, o - Cc + r0, c + r1, ob + r2, g + r3, r0, r1 + 1, r2, r3))
+        if o >= Co1 and c >= Co2:
             Q.append(
-                (
-                    t - 1,
-                    m0 - v[4] + r0,
-                    m1 + r1,
-                    m2 - v[5] + r2,
-                    m3 + r3,
-                    r0,
-                    r1,
-                    r2,
-                    r3 + 1,
-                )
+                (t - 1, o - Co1 + r0, c - Co2 + r1, ob + r2, g + r3, r0, r1, r2 + 1, r3)
             )
-            continue
-        if m0 >= v[2] and m1 >= v[3]:
+        if o >= Cg1 and ob >= Cg2:
             Q.append(
-                (
-                    t - 1,
-                    m0 - v[2] + r0,
-                    m1 - v[3] + r1,
-                    m2 + r2,
-                    m3 + r3,
-                    r0,
-                    r1,
-                    r2 + 1,
-                    r3,
-                )
+                (t - 1, o - Cg1 + r0, c + r1, ob - Cg2 + r2, g + r3, r0, r1, r2, r3 + 1)
             )
-            continue
-        if m0 >= v[1]:
-            Q.append(
-                (
-                    t - 1,
-                    m0 - v[1] + r0,
-                    m1 + r1,
-                    m2 + r2,
-                    m3 + r3,
-                    r0,
-                    r1 + 1,
-                    r2,
-                    r3,
-                )
-            )
-        if m0 >= v[0]:
-            Q.append(
-                (
-                    t - 1,
-                    m0 - v[0] + r0,
-                    m1 + r1,
-                    m2 + r2,
-                    m3 + r3,
-                    r0 + 1,
-                    r1,
-                    r2,
-                    r3,
-                )
-            )
-        Q.append((t - 1, m0 + r0, m1 + r1, m2 + r2, m3 + r3, r0, r1, r2, r3))
-    total_geode += geo_max * int(k)
-print(total_geode)
+    return best
+
+
+lines = open("19.in").read().strip().split("\n")
+for line in lines:
+    id_, ore, clay, ob_ore, ob_clay, g_ore, g_ob = [
+        int(b.strip(":")) for b in line.split(" ") if b.strip(":").isnumeric()
+    ]
+
+    best = solve(ore, clay, ob_ore, ob_clay, g_ore, g_ob, 24)
+    quality_level = best * id_
