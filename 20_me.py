eaa6b9d97f7b68319209354d8226f383b7369cd3
Keunbae Park <pkboom@users.noreply.github.com>
2023-02-16 18:04:33 -0500

    wip

--- 20_me.py
+++ 20_me.py
@@ -26,19 +26,19 @@ while True:
     # get one number from S
     n = S[c % len(S)]
     m_key = M.index(n)
-    m_new_key = m_key + n
-    if m_new_key < 0:
-        m_new_key = (m_new_key % len(M)) - 1
+    if m_key + n >= len(M):
+        m_new_key = m_key + n + 1
+    elif m_key + n <= 0:
+        m_new_key = m_key + n - 1
     else:
-        m_new_key = m_new_key % len(M)
+        m_new_key = m_key + n
 
-    c2 = 0
-    while c2 < len(M):
-        
-    for i in range(m_key, len(M)):
+    m_new_key = m_new_key % len(M)
+
+    for i in range(m_key, m_key + len(M)):
         if (i % len(M)) == m_new_key:
             break
-        M[i], M[i + 1] = M[i + 1], M[i]
+        M[i % len(M)], M[(i + 1) % len(M)] = M[(i + 1) % len(M)], M[i % len(M)]
     c += 1
 
 print(p1)
