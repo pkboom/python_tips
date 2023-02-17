e3ff59d97195e454d92fe18a351f0bc7fe8eccd0
Keunbae Park <pkboom@users.noreply.github.com>
2023-02-17 13:05:03 -0500

    wip

--- 20_me.py
+++ 20_me.py
@@ -9,36 +9,43 @@ for line in lines:
     S.append(int(line))
     M.append(int(line))
 
+length = len(S)
 c = 0
 p1 = 0
 p2 = 0
 while True:
     if c == 1000:
-        print(M[(M.index(0) + 1) % len(M)])
-        p1 += M[(M.index(0) + 1) % len(M)]
+        print(M)
+        print(M[(M.index(0) + 1) % length])
+        p1 += M[(M.index(0) + 1) % length]
     if c == 2000:
-        print(M[(M.index(0) + 1) % len(M)])
-        p1 += M[(M.index(0) + 1) % len(M)]
+        print(M)
+        print(M[(M.index(0) + 1) % length])
+        p1 += M[(M.index(0) + 1) % length]
     if c == 3000:
-        print(M[(M.index(0) + 1) % len(M)])
-        p1 += M[(M.index(0) + 1) % len(M)]
+        print(M)
+        print(M[(M.index(0) + 1) % length])
+        p1 += M[(M.index(0) + 1) % length]
         break
-    # get one number from S
-    n = S[c % len(S)]
-    m_key = M.index(n)
-    if m_key + n >= len(M):
-        m_new_key = m_key + n + 1
-    elif m_key + n <= 0:
-        m_new_key = m_key + n - 1
+    current = S[c % len(S)]
+    print(c % len(S))
+    m_key = M.index(current)
+    if m_key + current > length:
+        m_new_key = m_key + current + 1
+    elif m_key + current < 0:
+        m_new_key = m_key + current - 1
     else:
-        m_new_key = m_key + n
+        m_new_key = m_key + current
 
-    m_new_key = m_new_key % len(M)
+    m_new_key = m_new_key % length
 
-    for i in range(m_key, m_key + len(M)):
-        if (i % len(M)) == m_new_key:
-            break
-        M[i % len(M)], M[(i + 1) % len(M)] = M[(i + 1) % len(M)], M[i % len(M)]
+    if m_new_key >= m_key:
+        step = 1
+    else:
+        step = -1
+
+    for i in range(m_key, m_new_key, step):
+        M[i % length], M[(i + step) % length] = M[(i + step) % length], M[i % length]
     c += 1
 
 print(p1)
