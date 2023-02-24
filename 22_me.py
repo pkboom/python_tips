69562d3d5332bc161d076d11d82d05bb36a97ee1
Keunbae Park <pkboom@users.noreply.github.com>
2023-02-24 17:00:51 -0500

    wip

--- 22_me.py
+++ 22_me.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 lines = open("example.in").read().split("\n\n")
-# lines = open("22.in").read().split("\n\n")
+lines = open("22.in").read().split("\n\n")
 
 map = [line for line in lines[:-1][0].split("\n")]
 path = lines[-1]
@@ -9,41 +9,47 @@ m_h = len(map)
 m_w = max([len(line) for line in map])
 move = (1, 0)
 D = ((1, 0), (0, 1), (-1, 0), (0, -1))
+# n_square = 4
+M = {}
+for l, line in enumerate(lines[:-1][0].split("\n")):
+    M[l] = []
+    for i in range(4):
+        M[l].append(line[i * (m_w // 4) : i * (m_w // 4) + (m_w // 4)])
+
+print(M)
+# def get_direction(d):
+#     return D[(D.index(move) + 1) % 4] if d == "R" else D[(D.index(move) - 1) % 4]
 
 
-def get_direction(d):
-    return D[(D.index(move) + 1) % 4] if d == "R" else D[(D.index(move) - 1) % 4]
+# i = 0
+# step = ""
+# p = [0, 0]  # x, y
+# while i < len(path):
+#     if path[i] == "R" or path[i] == "L":
+#         move = get_direction(path[i])
+#     else:
+#         step += path[i]
+#     if i == len(path) - 1 or path[i + 1] == "R" or path[i + 1] == "L":
+#         c, r = move
+#         dc = 0
+#         while dc < int(step):
+#             p[1] = (p[1] + r) % m_h
+#             p[0] = (p[0] + c) % m_w
+#             try:
+#                 t = map[p[1]][p[0]]
+#             except:
+#                 continue
+#             if t == ".":
+#                 x_tile, y_tile = p
+#                 dc += 1
+#             elif t == "#":
+#                 p = [x_tile, y_tile]
+#                 break
+#         step = ""
+#     i += 1
 
-
-i = 0
-step = ""
-p = [0, 0]  # x, y
-while i < len(path):
-    if path[i] == "R" or path[i] == "L":
-        move = get_direction(path[i])
-    else:
-        step += path[i]
-    if i == len(path) - 1 or path[i + 1] == "R" or path[i + 1] == "L":
-        c, r = move
-        dc = 0
-        while dc < int(step):
-            p[1] = (p[1] + r) % m_h
-            p[0] = (p[0] + c) % m_w
-            try:
-                t = map[p[1]][p[0]]
-            except:
-                continue
-            if t == ".":
-                x_tile, y_tile = p
-                dc += 1
-            elif t == "#":
-                p = [x_tile, y_tile]
-                break
-        step = ""
-    i += 1
-
-print(p)
-x, y = p
-x += 1
-y += 1
-print(1000 * y + 4 * x + D.index(move))
+# print(p)
+# x, y = p
+# x += 1
+# y += 1
+# print(1000 * y + 4 * x + D.index(move))
