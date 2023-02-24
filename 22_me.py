6bbb13e094939b2ff909593fe46d74260236b733
Keunbae Park <pkboom@users.noreply.github.com>
2023-02-24 14:44:58 -0500

    wip

--- 22_me.py
+++ 22_me.py
@@ -1,11 +1,12 @@
 from collections import deque
 
 lines = open("example.in").read().split("\n\n")
-# lines = open("22.in").read().split("\n")
+# lines = open("22.in").read().split("\n\n")
 
 map = [line for line in lines[:-1][0].split("\n")]
 path = lines[-1]
-
+m_h = len(map)
+m_w = max([len(line) for line in map])
 move = (1, 0)
 D = ((1, 0), (0, 1), (-1, 0), (0, -1))
 
@@ -16,7 +17,7 @@ def get_direction(d):
 
 i = 0
 step = ""
-p = [0, 0]
+p = [0, 0]  # x, y
 while i < len(path):
     if path[i] == "R" or path[i] == "L":
         move = get_direction(path[i])
@@ -26,18 +27,23 @@ while i < len(path):
         c, r = move
         dc = 0
         while dc < int(step):
-            p[0] = (p[0] + c) % len(map[0])
-            p[1] = (p[1] + r) % len(map)
-            t = map[p[1]][p[0]]
-            if t == " ":
+            p[1] = (p[1] + r) % m_h
+            p[0] = (p[0] + c) % m_w
+            try:
+                t = map[p[1]][p[0]]
+            except:
                 continue
             if t == ".":
-                print(p)
                 x_tile, y_tile = p
                 dc += 1
             elif t == "#":
                 p = [x_tile, y_tile]
                 break
-        print(p)
         step = ""
     i += 1
+
+print(p)
+x, y = p
+x += 1
+y += 1
+print(1000 * y + 4 * x + D.index(move))
