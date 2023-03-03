b16f6cdf89cd3d3281f8242a19cedca492b3baa3
Keunbae Park <pkboom@users.noreply.github.com>
2023-03-03 16:02:10 -0500

    wip

--- 23_me.py
+++ 23_me.py
@@ -3,6 +3,7 @@ from collections import deque
 lines = open("23.in").read().strip().split("\n")
 # lines = open("example.in").read().strip().split("\n")
 
+
 E = deque()
 for j, line in enumerate(lines):
     for i, l in enumerate(line):
@@ -17,9 +18,16 @@ P = [
     ((-1,1), (-1,0), (-1,-1)),
     ((1,-1), (1,0), (1,1)),
 ]
+next_positions = deque()
 new_positions = deque()
 
-for _ in range(10):
+while True:
+    if mc == 10:
+        y_val = y_max - y_min + 1
+        x_val = x_max - x_min + 1
+        print(x_val * y_val - len(E)) 
+        # print(empty)
+        # break
     for ex,ey in E:
         for dx, dy in ((-1,0), (-1,-1), (0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1)):
             nx, ny = ex + dx, ey + dy
@@ -27,13 +35,12 @@ for _ in range(10):
                 # search for a new position
                 break
         else:
-            print("elf", (ex,ey), "is no elves around it")
+            # print("elf", (ex,ey), "is no elves around it")
             new_positions.append((ex,ey))
             continue
     
         # search for a new position
-        mmc = mc % 4
-        for i in range(mmc, mmc+4):
+        for i in range(mc, mc+4):
             # search one side 
             side = P[i % 4]
             for dx, dy in side :
@@ -41,44 +48,34 @@ for _ in range(10):
                 if (nx,ny) in E:
                     break
             else:
-                # we have an empty line
-                print('(ex, ey) (dx, dy), (nx, ny)', (ex, ey), side[1], (ex + side[1][0], ey+side[1][1]))
-                new_positions.append((ex + side[1][0], ey+side[1][1]))
+                # found a new position
+                # print('(ex, ey) (dx, dy), (nx, ny)', (ex, ey), side[1], (ex + side[1][0], ey+side[1][1]))
+                new_positions.append((ex + side[1][0], ey + side[1][1]))
                 break
         else:
             # print((ex,ey), "surrounded by elves")
             new_positions.append((ex,ey))
     
+    if mc % 100 == 0:
+        print('mc', mc)
+    if E == new_positions:
+        print("no change", mc + 1)
+        break
     
-    for _ in range(len(new_positions)):
-        np = new_positions.popleft()
+    for np in new_positions:
         op = E.popleft()
-        if np in new_positions:
-            print("we have duplicate new positions", np)
-            new_positions.append(np)
+        if new_positions.count(np) > 1:
+            # print("we have duplicate new positions", np)
             E.append(op) 
         else:
-            print("new position", np) 
+            # print("new position", np) 
             E.append(np) 
     
     x_max = max(x for x, _ in E)
     x_min = min(x for x, _ in E)
     y_max = max(y for _, y in E)
     y_min = min(y for _, y in E)
-    print(x_min, x_max, y_min, y_max)
 
     mc += 1
     new_positions.clear()
-    empty = 0
-    for j in range(y_min, y_max+1):
-        a = ''
-        for i in range(x_min, x_max+1):
-            if (i,j) in E:
-                a += '#'
-            else:
-                a += '.'
-                empty += 1
-        print(a)
-    print('----------------------------')
 
-print(empty)
